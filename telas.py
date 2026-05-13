611ce294ef3724a467dc4e651abdfd76965e7db6
Marilia Frota <mariliafrota11@outlook.com>
2026-05-13 10:32:13 -0300

    Correção_tela_inicial

--- telas.py
+++ telas.py
@@ -35,19 +35,3 @@ def tela_inicial():
         clock.tick(60)
 
 # chama a tela inicial
-tela_inicial()
-
-pygame.quit()
-def tela_vitoria ():
-    fonte = pygame.fonte.SysFont("arial",50,True)
-    while True:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                return "sair"
-            if evento.type == pygame.KEYDOWN:
-                return "inicio"
-        tela.fill((200,255,200))
-        texto = fonte.render("Você salvou o Botas!", True, (0,0,0))
-        texto.blit (texto, (250,300))
-        pygame.display.flip()
-        clock.tick(60)
