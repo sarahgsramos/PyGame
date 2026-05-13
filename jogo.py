8cd84195bffb6cf6adbbab03c39ecfc5289fa39e
Marilia Frota <mariliafrota11@outlook.com>
2026-05-13 10:39:35 -0300

    Início_lógica

---
+++ jogo.py
@@ -0,0 +1,9 @@
+from telas import tela_inicial
+from tela_vitoria import tela_vitoria
+
+estado = "inicio"
+while True:
+    if estado == "inicio":
+        estado = tela_inicial
+    elif estado == "sair":
+        break
