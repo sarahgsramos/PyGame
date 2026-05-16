e0cf7323d97d296d374b889ce3ab574bc522c203
Sarah Ramos <sarahgsramoss2@gmaill.com>
2026-05-16 13:10:37 -0300

    Teste correção

--- jogo.py
+++ jogo.py
@@ -1,23 +1,29 @@
 from tela_inicial import tela_inicial
 from tela_vitoria import tela_vitoria
 from tela_derrota import tela_derrota
-import pygame  
+from jogo_fase import fase_jogo
+import pygame
 import sys
 pygame.init()
-
-
+ 
+ 
 LARGURA, ALTURA = 1100, 700
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 estado = "inicio"
+ 
 while True:
     if estado == "inicio":
         estado = tela_inicial(tela)
+        if estado == "fase":
+            estado = "jogo"
     elif estado == "jogo":
-        print("jogo rodando")
-        break
+        estado = fase_jogo(tela)
     elif estado == "vitoria":
         estado = tela_vitoria(tela)
     elif estado == "derrota":
         estado = tela_derrota(tela)
     elif estado == "sair":
         break
+ 
+pygame.quit()
+sys.exit()
